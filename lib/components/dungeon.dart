f506dfee205bc0c66e53febb1800bfb80ceff18e
tnantoka <tnantoka@bornneet.com>
2023-07-13 00:03:58 +0900

    feat: change map size

--- lib/components/dungeon.dart
+++ lib/components/dungeon.dart
@@ -4,16 +4,17 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class Dungeon extends PositionComponent with HasGameRef {
-  final columns = 33;
-  final rows = 27;
+  static const seedScale = 7;
+  final columns = 11 * seedScale;
+  final rows = 9 * seedScale;
   final random = Random();
 
   late final RectangleComponent spritesContainer;
   late final RectangleComponent guideContainer;
   late final List<List<int>> map;
 
-  get seedColumns => columns ~/ 3;
-  get seedRows => rows ~/ 3;
+  get seedColumns => columns ~/ seedScale;
+  get seedRows => rows ~/ seedScale;
   get length => (game.size.y * 0.04).round();
 
   @override
@@ -80,28 +81,30 @@ class Dungeon extends PositionComponent with HasGameRef {
 
     for (int i = 1; i < seedRows - 1; i++) {
       for (int j = 1; j < seedColumns - 1; j++) {
-        final x = (j * 3).toInt() + 1;
-        final y = (i * 3).toInt() + 1;
+        final x = (j * seedScale).toInt() + 1;
+        final y = (i * seedScale).toInt() + 1;
         if (seed[i][j] == 0) {
           if (random.nextInt(100) < 20) {
-            for (int i2 = -1; i2 <= 1; i2++) {
-              for (int j2 = -1; j2 <= 1; j2++) {
+            for (int i2 = -(seedScale ~/ 2); i2 <= (seedScale ~/ 2); i2++) {
+              for (int j2 = -(seedScale ~/ 2); j2 <= (seedScale ~/ 2); j2++) {
                 map[y + i2][x + j2] = 0;
               }
             }
           } else {
             map[y][x] = 0;
-            if (seed[i - 1][j] == 0) {
-              map[y - 1][x] = 0;
-            }
-            if (seed[i + 1][j] == 0) {
-              map[y + 1][x] = 0;
-            }
-            if (seed[i][j - 1] == 0) {
-              map[y][x - 1] = 0;
-            }
-            if (seed[i][j + 1] == 0) {
-              map[y][x + 1] = 0;
+            for (int k = 1; k <= (seedScale ~/ 2); k++) {
+              if (seed[i - 1][j] == 0) {
+                map[y - k][x] = 0;
+              }
+              if (seed[i + 1][j] == 0) {
+                map[y + k][x] = 0;
+              }
+              if (seed[i][j - 1] == 0) {
+                map[y][x - k] = 0;
+              }
+              if (seed[i][j + 1] == 0) {
+                map[y][x + k] = 0;
+              }
             }
           }
         }
